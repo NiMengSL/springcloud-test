d3c12625c3033174220fa889a58e55a48d5c48f1
tomcat <4161848zsZS>
2023-07-25 16:37:04 +0800

    update

--- cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyLogGateWayFilter.java
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyLogGateWayFilter.java
@@ -5,13 +5,14 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.Date;
 
 @Slf4j
-// @Component
+@Component
 public class MyLogGateWayFilter implements GlobalFilter, Ordered {
 
     @Override
